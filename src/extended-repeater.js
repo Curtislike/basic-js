5b342d146428fb76979f212c0bf2d760d23f75c7
Curtislike <anna-olesh@mail.ru>
2021-10-03 18:43:05 +0300

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -17,24 +17,32 @@ import { NotImplementedError } from "../extensions/index.js";
  */
 export default function repeater(str, options) {
   if (typeof str !== "string") str = String(str);
-  if (options.addition === undefined) {
+
+  if (!options.addition) {
     options.addition = "";
   } else {
     if (typeof options.addition !== "string")
       options.addition = String(options.addition);
   }
-  if (options.separator === undefined) options.separator = "+";
-  if (options.additionSeparator === undefined) options.additionSeparator = "";
-  if (options.repeatTimes === undefined) options.repeatTimes = 1;
-  if (options.additionRepeatTimes === undefined)
-    options.additionRepeatTimes = 1;
-  let repeatedAddition =
-    (options.addition + options.additionSeparator).repeat(
-      options.additionRepeatTimes
-    ) + options.addition;
+  if (!options.separator) options.separator = "+";
+  if (!options.additionSeparator) options.additionSeparator = "";
+  if (!options.repeatTimes) options.repeatTimes = 1;
+  if (!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+
+  let repeatedAddition = (options.addition + options.additionSeparator).repeat(
+    options.additionRepeatTimes
+  );
+
   let transformedStr = str + repeatedAddition;
+
   let repeatedStr =
     (transformedStr + options.separator).repeat(options.repeatTimes - 1) +
     transformedStr;
+
+  // let repeatedStr =
+  //   (transformedStr + (options.repeatTimes - 1)
+  //     ? options.separator
+  //     : ""
+  //   ).repeat(options.repeatTimes - 1) + transformedStr;
   return repeatedStr;
 }
